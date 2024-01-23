1a22331e301e9ff218110e03b0716ce83c5bd8dd
jprigotti <julian.nardi@gmail.com>
2024-01-23 17:36:17 -0300

    Add languages to HomeHero text

--- src/pages/home/view/HomeView.jsx
+++ src/pages/home/view/HomeView.jsx
@@ -5,12 +5,17 @@ import HomeServices from '../components/HomeServices'
 import HomeContact from '../components/HomeContact'
 import HomeHero from '../components/HomeHero'
 
+import { useTranslation } from 'react-i18next'
+
 const HomeView = () => {
+
+  const { t } = useTranslation('translation', { keyPrefix: 'home' });
+
   return (
     <div>
       <HomeHero
-        heroTitle='¿Tenes dudas sobre tu diagnóstico?'
-        heroParagraph='Consulta por una segunda opinión especializada en imágenes'
+        heroTitle={t(`home_hero_title`)}
+        heroParagraph={t(`home_hero_paragraph`)}
       />
       <HomeAbout />
       <HomeServices />
