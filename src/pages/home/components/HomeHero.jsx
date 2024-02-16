662137b4dfc2ffb0d75247715c3fdd3e01c0cc2f
titojp <titojp2002@hotmail.com>
2024-02-16 02:53:38 -0300

    updated hero text

--- src/pages/home/components/HomeHero.jsx
+++ src/pages/home/components/HomeHero.jsx
@@ -6,7 +6,7 @@ const HomeHero = ({ heroTitle, heroParagraph }) => {
     <div className="hero">
       <div className='hero--container'>
         <div className="hero--container-body">
-          <h2 className='titleSize2 titleStyle1 '>{heroTitle}</h2>
+          <h2 className='titleSize2 titleStyle1 textShadow1 '>{heroTitle}</h2>
           <p className='textSize3 textStyle1'>{heroParagraph}</p>
         </div>
       </div>
